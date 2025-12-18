51e12cf40915974446d26ba2068342d7e552cb5b
TurtleTSwt <huysdsd23@gmail.com>
2025-12-18 10:45:06 +0700

    a

--- src/main/java/com/teamforone/tech_store/config/ApplicationConfig.java
+++ src/main/java/com/teamforone/tech_store/config/ApplicationConfig.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-//package com.teamforone.tech_store.config;
-//
-////import com.teamforone.tech_store.repository.admin.RBAC.NhanVienRepository;
-//import com.teamforone.tech_store.repository.admin.crud.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class ApplicationConfig {
-//    private final UserRepository repository;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        return username ->
-//                repository.findByUsername(username)
-//                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//    }
-//}
-=======
 package com.teamforone.tech_store.config;
 
 import com.teamforone.tech_store.repository.admin.RBAC.NhanVienRepository;
@@ -71,4 +33,3 @@ public class ApplicationConfig {
                         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
->>>>>>> a4a014af87307103265b91d7afcd2e53131a4ebb
